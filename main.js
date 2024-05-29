be598dfd5da622a3462ef51068aee18cafc27f62
inzilion <inzilion@gmail.com>
2024-05-29 15:35:19 +0900

    add link

--- main.js
+++ main.js
@@ -1,80 +1,40 @@
 const { app, BrowserWindow, Menu } = require('electron/main')
 const path = require('node:path')
 const template = [
-    // { role: 'appMenu' }
-    {
-          label: 'CSLB',
-          submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'quit' }
-          ]
-    },
-    // { role: 'fileMenu' }
-    {
-      label: '사이트',
-      submenu: [
-        { label: '코드업',
-          click: async () => {
-            const { shell } = require('electron')
-            await shell.openExternal('https://codeup.kr')
-          }
+  // { role: 'appMenu' }
+  {
+    label: 'CSLB',
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  },
+  // { role: 'fileMenu' }
+  {
+    label: '사이트',
+    submenu: [
+      { label: '코드업',
+        click: async () => {
+        const win = new BrowserWindow({ width: 1024, height: 768 })
+        win.loadURL('https://codeup.kr')          }
+      },
+      { label: 'BIKO',
+        click: async () => {
+        const win = new BrowserWindow({ width: 1024, height: 768 })
+        win.loadURL('https://biko.kr')          }
         },
-        { label: 'BIKO' },
-        { label: 'KOISTUDY' }
-      ]
-    },
-    // { role: 'editMenu' }
-    {
-      label: 'Edit',
-      submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-      ]
-    },
-    // { role: 'viewMenu' }
-    {
-      label: 'View',
-      submenu: [
-        { role: 'reload' },
-        { role: 'forceReload' },
-        { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
-      ]
-    },
-    // { role: 'windowMenu' }
-    {
-      label: 'Window',
-      submenu: [
-        { role: 'minimize' },
-        { role: 'zoom' },
-      ]
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click: async () => {
-            const { shell } = require('electron')
-            await shell.openExternal('https://electronjs.org')
-          }
-        }
-      ]
-    }
-  ]
-  
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
+      { label: 'KOISTUDY',
+        click: async () => {
+        const win = new BrowserWindow({ width: 1024, height: 768 })
+        win.loadURL('http://koistudy.net')          }
+      }
+    ]
+  },
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 const createWindow = () => {
   const win = new BrowserWindow({
